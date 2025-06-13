ac25c20fa65ce5fbd21f921f231c9c13f1906e85
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-06-14 00:25:20 +0400

    bootstrap, imask, swiper

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,3 +1,27 @@
+import IMask from 'imask';
+import Modal from 'bootstrap/js/dist/modal';
 import '@/components/header/header.js';
 import '@/components/productsNav/productsNav.js';
 import '@/components/cookieBlock/cookieBlock.js';
+
+const maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+
+const phoneInput = document.querySelector('.js-phone-input');
+const feedbackModalElement = document.getElementById('feedbackModal');
+
+let mask;
+
+if (phoneInput) {
+  mask = IMask(phoneInput, maskOptions);
+}
+
+feedbackModalElement.addEventListener('show.bs.modal', evt => {
+  let heading = feedbackModalElement.querySelector('.modal-title');
+  heading.textContent = evt.relatedTarget.innerText;
+})
+
+feedbackModalElement.addEventListener('shown.bs.modal', evt => {
+  phoneInput.focus();
+})
